6924176c8828de068f5eb7c951b627303ef20e32
hocaron <pooh0407@ewhain.net>
2022-04-29 01:45:51 +0900

    feat : store 관련 dto

--- src/store/dto/create-store.dto.ts
+++ src/store/dto/create-store.dto.ts
@@ -1,1 +1,20 @@
-export class CreateStoreDto {}
+import {ApiProperty} from '@nestjs/swagger';
+import {BasePostReponseBodyDto} from '../../common/dtos/base-post-response-body.dto';
+
+export class CreateStoreDto {
+  @ApiProperty({example: 'sixshop'})
+  name: string;
+}
+
+export class CreateStoreResponseDto {
+  @ApiProperty({example: 'sixshop'})
+  name: string;
+
+  @ApiProperty({example: 'a1s2d3f4g5'})
+  _id: string;
+}
+
+export class CreateStoreResponseBodyDto extends BasePostReponseBodyDto {
+  @ApiProperty()
+  data: CreateStoreResponseDto;
+}
