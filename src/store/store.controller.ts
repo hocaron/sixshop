c29a613a5480a824ad1cc4ac01f8d36af319c9bb
hocaron <pooh0407@ewhain.net>
2022-04-29 01:43:31 +0900

    feat : store controller 생성

--- src/store/store.controller.ts
+++ src/store/store.controller.ts
@@ -2,33 +2,41 @@ import {Controller, Get, Post, Body, Patch, Param, Delete} from '@nestjs/common'
 import {StoreService} from './store.service';
 import {CreateStoreDto} from './dto/create-store.dto';
 import {UpdateStoreDto} from './dto/update-store.dto';
+import {ApiTags} from '@nestjs/swagger';
+import {docs} from './store.docs';
 
+@ApiTags('store')
 @Controller('store')
 export class StoreController {
   constructor(private readonly storeService: StoreService) {}
 
   @Post()
-  create(@Body() createStoreDto: CreateStoreDto) {
-    return this.storeService.create(createStoreDto);
+  @docs.createStore('상점 생성')
+  createStore(@Body() createStoreDto: CreateStoreDto) {
+    return this.storeService.createStore(createStoreDto);
   }
 
   @Get()
-  findAll() {
-    return this.storeService.findAll();
+  @docs.getAllStores('모든 상점 조회')
+  getAllStores() {
+    return this.storeService.getAllStores();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.storeService.findOne(+id);
+  @docs.getStore('ID로 상점 조회')
+  getStore(@Param('id') id: string) {
+    return this.storeService.getStore(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateStoreDto: UpdateStoreDto) {
-    return this.storeService.update(+id, updateStoreDto);
+  @docs.updateStore('ID로 상점 정보 업데이트')
+  updateStore(@Param('id') id: string, @Body() updateStoreDto: UpdateStoreDto) {
+    return this.storeService.updateStore(id, updateStoreDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.storeService.remove(+id);
+  @docs.deleteStore('ID로 상점 삭제')
+  deleteStore(@Param('id') id: string) {
+    return this.storeService.deleteStore(id);
   }
 }
