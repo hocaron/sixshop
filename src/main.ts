842a5383c17ff63dddf248bd16168d1d95181717
hocaron <pooh0407@ewhain.net>
2022-04-29 03:19:10 +0900

    chore : global validation pipe 추가

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import {ValidationPipe} from '@nestjs/common';
 import {NestFactory} from '@nestjs/core';
 import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 import {AppModule} from './app.module';
@@ -7,6 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix(API_PREFIX);
+  app.useGlobalPipes(new ValidationPipe({transform: true}));
 
   const config = new DocumentBuilder()
     .setTitle('Sixshop API docs')
