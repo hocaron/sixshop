55dace8c71b4d76fcbd3566b236543bca624c9ae
hocaron <pooh0407@ewhain.net>
2022-04-29 03:19:54 +0900

    feat : custom-field service 생성

--- src/custom-field/custom-field.service.ts
+++ src/custom-field/custom-field.service.ts
@@ -1,26 +1,60 @@
-import {Injectable} from '@nestjs/common';
-import {CreateCustomFieldDto} from './dto/create-custom-field.dto';
+import {Injectable, HttpException} from '@nestjs/common';
+import {CreateCustomFieldDto, CreateCustomFieldResponseDto} from './dto/create-custom-field.dto';
 import {UpdateCustomFieldDto} from './dto/update-custom-field.dto';
+import {InjectModel} from '@nestjs/mongoose';
+import {CustomField, CustomFieldDocument} from './schemas/custom-field.schema';
+import {Model} from 'mongoose';
+import {Err} from './../common/error';
+import {GetCustomFieldResponseDto} from './dto/get-custom-field.dto';
 
 @Injectable()
 export class CustomFieldService {
-  create(createCustomFieldDto: CreateCustomFieldDto) {
-    return 'This action adds a new customField';
+  constructor(
+    @InjectModel(CustomField.name) private readonly customFieldModel: Model<CustomFieldDocument>,
+  ) {}
+
+  createCustomField(
+    createCustomFieldDto: CreateCustomFieldDto,
+  ): Promise<CreateCustomFieldResponseDto> {
+    return new this.customFieldModel(createCustomFieldDto).save();
   }
 
-  findAll() {
-    return `This action returns all customField`;
+  getAllCustomField(): Promise<GetCustomFieldResponseDto[]> {
+    return this.customFieldModel.find().exec();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} customField`;
+  async getCustomField(id: string): Promise<GetCustomFieldResponseDto> {
+    const existingCustomField = await this.customFieldModel.findById(id).exec();
+    if (!existingCustomField) {
+      throw new HttpException(
+        Err.CUSTOM_FIELD.NOT_FOUND_CUSTOM_FIELD.message,
+        Err.CUSTOM_FIELD.NOT_FOUND_CUSTOM_FIELD.statusCode,
+      );
+    }
+    return existingCustomField;
   }
 
-  update(id: number, updateCustomFieldDto: UpdateCustomFieldDto) {
-    return `This action updates a #${id} customField`;
+  async updateCustomField(id: string, updateCustomFieldDto: UpdateCustomFieldDto): Promise<string> {
+    const existingCustomField = await this.customFieldModel.findById(id).exec();
+    if (!existingCustomField) {
+      throw new HttpException(
+        Err.CUSTOM_FIELD.NOT_FOUND_CUSTOM_FIELD.message,
+        Err.CUSTOM_FIELD.NOT_FOUND_CUSTOM_FIELD.statusCode,
+      );
+    }
+    existingCustomField.update(updateCustomFieldDto).exec();
+    return '업데이트에 성공하였습니다.';
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} customField`;
+  async removeCustomField(id: string): Promise<string> {
+    const existingCustomField = await this.customFieldModel.findById(id).exec();
+    if (!existingCustomField) {
+      throw new HttpException(
+        Err.CUSTOM_FIELD.NOT_FOUND_CUSTOM_FIELD.message,
+        Err.CUSTOM_FIELD.NOT_FOUND_CUSTOM_FIELD.statusCode,
+      );
+    }
+    existingCustomField.deleteOne({id});
+    return '삭제에 성공하였습니다.';
   }
 }
