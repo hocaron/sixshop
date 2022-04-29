098caf760e87c7cb87eaea33f0fdafe7748dd401
hocaron <pooh0407@ewhain.net>
2022-04-30 06:57:54 +0900

    feat : customer 관련 controller 생성

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -2,33 +2,30 @@ import {Controller, Get, Post, Body, Patch, Param, Delete} from '@nestjs/common'
 import {CustomerService} from './customer.service';
 import {CreateCustomerDto} from './dto/create-customer.dto';
 import {UpdateCustomerDto} from './dto/update-customer.dto';
+import {ApiTags} from '@nestjs/swagger';
 
+@ApiTags('customer')
 @Controller('customer')
 export class CustomerController {
   constructor(private readonly customerService: CustomerService) {}
 
   @Post()
-  create(@Body() createCustomerDto: CreateCustomerDto) {
-    return this.customerService.create(createCustomerDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.customerService.findAll();
+  createCustomer(@Body() createCustomerDto: CreateCustomerDto) {
+    return this.customerService.createCustomer(createCustomerDto);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.customerService.findOne(+id);
+  getCustomer(@Param('id') id: string) {
+    return this.customerService.getCustomer(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCustomerDto: UpdateCustomerDto) {
-    return this.customerService.update(+id, updateCustomerDto);
+  updateCustomer(@Param('id') id: string, @Body() updateCustomerDto: UpdateCustomerDto) {
+    return this.customerService.updateCustomer(id, updateCustomerDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.customerService.remove(+id);
+  deleteCustomer(@Param('id') id: string) {
+    return this.customerService.deleteCustomer(id);
   }
 }
