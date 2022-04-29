2ddcc9bc8e2bb59916613346b4b12a7e6198f9ac
hocaron <pooh0407@ewhain.net>
2022-04-30 05:15:53 +0900

    feat : customer, order, product 별 사용자 정의 필드 id를 가지고 있는 필드 추가

--- src/store/dto/create-store.dto.ts
+++ src/store/dto/create-store.dto.ts
@@ -1,14 +1,33 @@
 import {ApiProperty} from '@nestjs/swagger';
+import {CustomField} from 'src/custom-field/schemas/custom-field.schema';
 import {BasePostReponseBodyDto} from '../../common/dtos/base-post-response-body.dto';
 
 export class CreateStoreDto {
   @ApiProperty({example: 'sixshop', description: '상점 이름'})
   name: string;
+
+  @ApiProperty({example: 'sixshop', description: '고객 관련 사용자 정의 필드 ID'})
+  customerCustomFieldIds: CustomField[];
+
+  @ApiProperty({example: 'sixshop', description: '상품 관련 사용자 정의 필드 ID'})
+  productCustomFieldIds: CustomField[];
+
+  @ApiProperty({example: 'sixshop', description: '주문 관련 사용자 정의 필드 ID'})
+  orderCustomFieldIds: CustomField[];
 }
 
 export class CreateStoreResponseDto {
   @ApiProperty({example: 'sixshop', description: '상점 이름'})
   name: string;
+
+  @ApiProperty({example: 'sixshop', description: '고객 관련 사용자 정의 필드 ID'})
+  customerCustomFieldIds: CustomField[];
+
+  @ApiProperty({example: 'sixshop', description: '상품 관련 사용자 정의 필드 ID'})
+  productCustomFieldIds: CustomField[];
+
+  @ApiProperty({example: 'sixshop', description: '주문 관련 사용자 정의 필드 ID'})
+  orderCustomFieldIds: CustomField[];
 
   @ApiProperty({example: 'a1s2d3f4g5', description: '상점 ID'})
   _id: string;
--- src/store/schemas/store.schema.ts
+++ src/store/schemas/store.schema.ts
@@ -1,5 +1,7 @@
 import {Prop, Schema, SchemaFactory} from '@nestjs/mongoose';
+import mongoose from 'mongoose';
 import {Document} from 'mongoose';
+import {CustomField} from 'src/custom-field/schemas/custom-field.schema';
 
 export type StoreDocument = Store & Document;
 
@@ -10,6 +12,15 @@ export class Store {
 
   @Prop({unique: true})
   name: string;
+
+  @Prop({type: [{type: mongoose.Schema.Types.ObjectId, ref: 'CustomField'}]})
+  customerCustomFieldIds: CustomField[];
+
+  @Prop({type: [{type: mongoose.Schema.Types.ObjectId, ref: 'CustomField'}]})
+  productCustomFieldIds: CustomField[];
+
+  @Prop({type: [{type: mongoose.Schema.Types.ObjectId, ref: 'CustomField'}]})
+  orderCustomFieldIds: CustomField[];
 }
 
 export const StoreSchema = SchemaFactory.createForClass(Store);
