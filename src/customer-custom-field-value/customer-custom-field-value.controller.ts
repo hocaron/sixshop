67413b6aab6df663b863ae866a11f14c903eb758
hocaron <pooh0407@ewhain.net>
2022-04-30 06:59:05 +0900

    feat : customer-custom-field 관련 controller 생성

--- src/customer-custom-field-value/customer-custom-field-value.controller.ts
+++ src/customer-custom-field-value/customer-custom-field-value.controller.ts
@@ -2,36 +2,53 @@ import {Controller, Get, Post, Body, Patch, Param, Delete} from '@nestjs/common'
 import {CustomerCustomFieldValueService} from './customer-custom-field-value.service';
 import {CreateCustomerCustomFieldValueDto} from './dto/create-customer-custom-field-value.dto';
 import {UpdateCustomerCustomFieldValueDto} from './dto/update-customer-custom-field-value.dto';
+import {ApiTags} from '@nestjs/swagger';
 
+@ApiTags('customer-custom-field-value')
 @Controller('customer-custom-field-value')
 export class CustomerCustomFieldValueController {
   constructor(private readonly customerCustomFieldValueService: CustomerCustomFieldValueService) {}
 
   @Post()
-  create(@Body() createCustomerCustomFieldValueDto: CreateCustomerCustomFieldValueDto) {
-    return this.customerCustomFieldValueService.create(createCustomerCustomFieldValueDto);
+  createCustomerCustomFieldValue(
+    @Body() createCustomerCustomFieldValueDto: CreateCustomerCustomFieldValueDto,
+  ) {
+    return this.customerCustomFieldValueService.createCustomerCustomFieldValue(
+      createCustomerCustomFieldValueDto,
+    );
   }
 
-  @Get()
-  findAll() {
-    return this.customerCustomFieldValueService.findAll();
+  @Get(':customFieldId/:customerId')
+  getCustomerCustomFieldValueByCustomerAndStore(
+    @Param('customFieldId') customFieldId: string,
+    @Param('customerId') customerId: string,
+  ) {
+    return this.customerCustomFieldValueService.getCustomerCustomFieldValueByCustomerAndStore(
+      customFieldId,
+      customerId,
+    );
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.customerCustomFieldValueService.findOne(+id);
+  @Get(':customFieldId')
+  getAllCustomerCustomFieldValueInStore(@Param('customFieldId') customFieldId: string) {
+    return this.customerCustomFieldValueService.getAllCustomerCustomFieldValueInStore(
+      customFieldId,
+    );
   }
 
   @Patch(':id')
-  update(
+  updateCustomerCustomFieldValue(
     @Param('id') id: string,
     @Body() updateCustomerCustomFieldValueDto: UpdateCustomerCustomFieldValueDto,
   ) {
-    return this.customerCustomFieldValueService.update(+id, updateCustomerCustomFieldValueDto);
+    return this.customerCustomFieldValueService.updateCustomerCustomFieldValue(
+      id,
+      updateCustomerCustomFieldValueDto,
+    );
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.customerCustomFieldValueService.remove(+id);
+  deleteCustomerCustomFieldValue(@Param('id') id: string) {
+    return this.customerCustomFieldValueService.deleteCustomerCustomFieldValue(id);
   }
 }
