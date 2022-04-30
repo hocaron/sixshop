ce87eaf12cd8388d81c1aa1056f79df42cdacbce
hocaron <pooh0407@ewhain.net>
2022-04-30 09:58:20 +0900

    feat : product 관련 controller 및 서비스 생성

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -2,33 +2,41 @@ import {Controller, Get, Post, Body, Patch, Param, Delete} from '@nestjs/common'
 import {ProductService} from './product.service';
 import {CreateProductDto} from './dto/create-product.dto';
 import {UpdateProductDto} from './dto/update-product.dto';
+import {ApiTags} from '@nestjs/swagger';
+import {docs} from './product.docs';
 
+@ApiTags('product')
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
   @Post()
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productService.create(createProductDto);
+  @docs.createProduct('상품 생성')
+  createProduct(@Body() createProductDto: CreateProductDto) {
+    return this.productService.createProduct(createProductDto);
   }
 
-  @Get()
-  findAll() {
-    return this.productService.findAll();
+  @Get(':storeId')
+  @docs.getAllProductInStore('상점 내에 있는 상품 조회')
+  getAllProductInStore(@Param('storeId') storeId: string) {
+    return this.productService.getAllProductInStore(storeId);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.productService.findOne(+id);
+  @docs.getProduct('ID로 상품 조회')
+  getProduct(@Param('id') id: string) {
+    return this.productService.getProduct(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productService.update(+id, updateProductDto);
+  @docs.updateProduct('ID로 상품 정보 업데이트')
+  updateProduct(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
+    return this.productService.updateProduct(id, updateProductDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.productService.remove(+id);
+  @docs.deleteProduct('ID로 상품 삭제')
+  deleteProduct(@Param('id') id: string) {
+    return this.productService.deleteProduct(id);
   }
 }
