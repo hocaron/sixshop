663f9d2ef8522cd3d33a52295ad826150ac3e5eb
hocaron <pooh0407@ewhain.net>
2022-05-02 03:28:47 +0900

    refactor : mongo db 정보 config file로 생성

---
+++ src/common/config/mongo.config.ts
@@ -0,0 +1,8 @@
+import {registerAs} from '@nestjs/config';
+import {MongooseModuleOptions} from '@nestjs/mongoose';
+
+export default registerAs('mongo', async (): Promise<MongooseModuleOptions> => {
+  return {
+    uri: process.env.MONGO_URI || 'mongodb://root:root@localhost:27017/sixshop',
+  };
+});
