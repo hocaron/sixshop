3280b709327a37537c32fdf2a30d526d11d0f032
hocaron <pooh0407@ewhain.net>
2022-05-03 01:18:05 +0900

    chore : 환경변수 설정을 안 했을 시, docker compose 내에서 작동하도록 변경

--- src/common/config/mongo.config.ts
+++ src/common/config/mongo.config.ts
@@ -3,6 +3,6 @@ import {MongooseModuleOptions} from '@nestjs/mongoose';
 
 export default registerAs('mongo', async (): Promise<MongooseModuleOptions> => {
   return {
-    uri: process.env.MONGO_URI || 'mongodb://root:root@localhost:27017/sixshop',
+    uri: process.env.MONGO_URI || 'mongodb://root:root@mongo:27017/sixshop',
   };
 });
