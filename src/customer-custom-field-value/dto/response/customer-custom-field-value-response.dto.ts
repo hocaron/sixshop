b3d6cf8977ac933dd8232ef194aeb0a0398c92d3
hocaron <pooh0407@ewhain.net>
2022-05-03 02:01:40 +0900

    fix : customer-custom-field 도메인  버그 해결

--- src/customer-custom-field-value/dto/response/customer-custom-field-value-response.dto.ts
+++ src/customer-custom-field-value/dto/response/customer-custom-field-value-response.dto.ts
@@ -1,10 +1,11 @@
 import {ApiProperty} from '@nestjs/swagger';
 import {Customer} from '../../../customer/schemas/customer.schema';
 import {CustomField} from '../../../custom-field/schemas/custom-field.schema';
+import mongoose from 'mongoose';
 
 export class CustomerCustomFieldValueResponseDto {
   @ApiProperty({example: 'a1s2d3f4g5', description: '사용자 정의 필드 ID'})
-  _id: string;
+  id: mongoose.Types.ObjectId;
 
   @ApiProperty({example: '01012341234', description: '고객 관련 사용자 정의 필드의 값'})
   value: string;
