082733266e026d4e3c00f26b9d72a812513133a3
hocaron <pooh0407@ewhain.net>
2022-05-03 20:22:26 +0900

    docs : product 관련 docs 보완

--- src/product/dto/request/create-product-request.dto.ts
+++ src/product/dto/request/create-product-request.dto.ts
@@ -3,7 +3,7 @@ import {Store} from 'src/store/schemas/store.schema';
 import {Category} from 'src/category/schemas/category.schema';
 
 export class CreateProductRequestDto {
-  @ApiProperty({example: '옷', description: '상품 이름'})
+  @ApiProperty({example: 'testProoduct', description: '상품 이름'})
   name: string;
 
   @ApiProperty({example: '10000', description: '상품 가격'})
@@ -12,6 +12,6 @@ export class CreateProductRequestDto {
   @ApiProperty({example: '1a2s3d4f', description: '상점 ID'})
   storeId: Store;
 
-  @ApiProperty({example: '[1a2s3d4f]', description: '카테고리들 ID'})
+  @ApiProperty({example: '["1a2s3d4f"]', description: '카테고리들 ID'})
   categoryIds: Category[];
 }
--- src/product/dto/response/product-response.dto.ts
+++ src/product/dto/response/product-response.dto.ts
@@ -7,7 +7,7 @@ export class ProductResponseDto {
   @ApiProperty({example: 'a1s2d3f4g5', description: '상품 ID'})
   id: mongoose.Types.ObjectId;
 
-  @ApiProperty({example: '옷', description: '상품 이름'})
+  @ApiProperty({example: 'testProoduct', description: '상품 이름'})
   name: string;
 
   @ApiProperty({example: '10000', description: '상품 가격'})
@@ -16,6 +16,6 @@ export class ProductResponseDto {
   @ApiProperty({example: '1a2s3d4f', description: '상점 ID'})
   storeId: Store;
 
-  @ApiProperty({example: '[1a2s3d4f]', description: '카테고리들 ID'})
+  @ApiProperty({example: '["1a2s3d4f"]', description: '카테고리들 ID'})
   categoryIds: Category[];
 }
